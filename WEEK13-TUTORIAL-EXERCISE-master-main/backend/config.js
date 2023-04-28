58ef4b590769cf0e5e9497f1a010c1a4f88bfd47
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-04-28 12:18:31 +0700

    first init

--- WEEK13-TUTORIAL-EXERCISE-master-main/backend/config.js
+++ WEEK13-TUTORIAL-EXERCISE-master-main/backend/config.js
@@ -3,8 +3,9 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'secret',
-  database: 'webpro',
+  password: 'root',
+  port:8889,
+  database: 'webproweek13',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
--- WEEK13-TUTORIAL-EXERCISE-master-main/frontend/src/main.js
+++ WEEK13-TUTORIAL-EXERCISE-master-main/frontend/src/main.js
@@ -5,13 +5,13 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
+import Vuelidate from 'vuelidate'
 import 'bulma/css/bulma.css'
 
 library.add(fas)
 
 Vue.component('icon', FontAwesomeIcon)
-
+Vue.use(Vuelidate)
 Vue.config.productionTip = false
 
 new Vue({
