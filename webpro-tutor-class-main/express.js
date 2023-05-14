9753e59b04fe37c0f67a78cee6d503599266c641
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-05-15 03:01:08 +0700

    Update express.js

--- webpro-tutor-class-main/express.js
+++ webpro-tutor-class-main/express.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/todo", async (req, res, next) => {
 	const schema = Joi.object({
+		//define schema + validate
 		title: Joi.string().required().messages({
 			"string.empty": "ต้องกรอก title",
 		}),
@@ -16,9 +17,9 @@ app.post("/todo", async (req, res, next) => {
 		}),
 		due_date: Joi.date().iso(),
 	});
-
+	// const result = schema.validate(req.body);
 	const { value, error } = schema.validate(req.body);
-
+	// check error
 	if (error) {
 		return res.status(400).json({
 			message: error.details[0].message,
@@ -30,10 +31,10 @@ app.post("/todo", async (req, res, next) => {
 
 	try {
 		const { title, description, due_date } = value;
-
+		// max order in list
 		const [max] = await pool.query("SELECT MAX(`order`) AS maxorder FROM todo");
 		const order = max[0].maxorder + 1;
-		const now = new Date().toISOString().slice(0, 10);
+		const now = new Date().toISOString().slice(0, 10); // format date to yyyy-mm-dd and create date now for default if not set due_date
 
 		const [result] = await connection.query(
 			"INSERT INTO todo (title, description, due_date, `order`) VALUES (?, ?, ?, ?)",
@@ -49,7 +50,7 @@ app.post("/todo", async (req, res, next) => {
 				title,
 				description,
 				due_date: due_date
-					? new Date(due_date).toISOString().slice(0, 10)
+					? new Date(due_date).toISOString().slice(0, 10) // format date
 					: now,
 				order,
 			},
@@ -75,8 +76,9 @@ app.delete("/todo/:id", async (req, res, next) => {
 		);
 		const [result] = await connection.query("DELETE FROM todo WHERE id = ?", [
 			id,
-		]);
-		await connection.commit();
+		]); // delete todo by id
+		await connection.commit(); 
+		// check result
 		if (result.affectedRows === 0) {
 			return res.status(404).json({
 				message: "ไม่พบ ToDo ที่ต้องการลบ",
@@ -95,28 +97,31 @@ app.delete("/todo/:id", async (req, res, next) => {
 	}
 });
 
+
+
 app.get("/todo", async (req, res, next) => {
 	let connection;
 	try {
 		const schema = Joi.object({
 			start_date: Joi.date(),
 			end_date: Joi.date().greater(Joi.ref("start_date")),
-		});
+		}); // define schema + validate
 
 		const { error } = schema.validate(req.query);
 		if (error) {
 			const err = new Error("Invalid query parameters");
 			err.status = 400;
 			throw err;
-		}
+		} // check error from validate schema
 
 		connection = await pool.getConnection();
 		await connection.beginTransaction();
 
-		const { start_date, end_date } = req.query;
-		let query = "SELECT * FROM todo";
-		let queryParams = [];
+		const { start_date, end_date } = req.query; // get query params
+		let query = "SELECT * FROM todo"; // set query 
+		let queryParams = []; // set query params
 
+		// check query params มีหรือไม่มีก็ได้
 		if (start_date && end_date) {
 			query += " WHERE due_date BETWEEN ? AND ?";
 			queryParams = [start_date, end_date];
@@ -128,18 +133,18 @@ app.get("/todo", async (req, res, next) => {
 			queryParams = [end_date];
 		}
 
-		query += " ORDER BY `order` ASC";
+		query += " ORDER BY `order` ASC"; // order by order ASC
 
-		const [result] = await connection.query(query, queryParams);
+		const [result] = await connection.query(query, queryParams); // query todo
 		await connection.commit();
 
 		const formattedTodos = result.map((todo) => {
 			const dueDate = new Date(todo.due_date);
 			dueDate.setDate(dueDate.getDate() + 1);
 			return {
-				...todo,
+				...todo, // spread operator คือการ copy ค่าจาก object นึงไปใส่ใน object อีกตัว
 				due_date: dueDate.toISOString().slice(0, 10),
-			};
+			}; // format date
 		});
 
 		res.status(200).json(formattedTodos);
