488b05db84f4ff8131b2884c09a29a266397b60c
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-05-14 23:32:25 +0700

    Update express.js

--- webpro-tutor-class-main/express.js
+++ webpro-tutor-class-main/express.js
@@ -6,44 +6,74 @@ const pool = require("./config/database");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const Joi = require("joi");
+
 app.post("/todo", async (req, res, next) => {
-    connection = await pool.getConnection();
-    await connection.beginTransaction();
-    try {
-      const { title, description, due_date } = req.body;
-      const [max] = await pool.query("SELECT MAX(`order`) AS maxorder FROM todo");
-      const order = max[0].maxorder + 1;
-      const now = new Date().toISOString().slice(0, 10);
-      const [result] = await connection.query(
-        "INSERT INTO todo (title, description, due_date, `order`) VALUES (?, ?, ?, ?)",
-        [title, description, due_date || now, order]
-      );
-      await connection.commit();
-      res.status(201).json({
-        message: `สร้าง ToDo '${title}' สำเร็จ`,
-        todo: {
-          id: result.insertId,
-          title,
-          description,
-          due_date: due_date ? new Date(due_date).toISOString().slice(0, 10) : now,
-          order,
-        },
-      });
-    } catch (error) {
-      if (connection) {
-        await connection.rollback();
-      }
-      next(error);
-    } finally {
-      connection.release();
-    }
+  const schema = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    due_date: Joi.date().iso(),
   });
 
+  const { error, value } = schema.validate(req.body);
+
+  let connection = await pool.getConnection();
+  await connection.beginTransaction();
+
+  try {
+    const { title, description, due_date } = value;
+    if (!title) {
+      return res.status(400).json({
+        message: "ต้องกรอก title",
+      });
+    }
+    if (!description) {
+      return res.status(400).json({
+        message: "ต้องกรอก description",
+      });
+    }
+    const [max] = await pool.query("SELECT MAX(`order`) AS maxorder FROM todo");
+    const order = max[0].maxorder + 1;
+    const now = new Date().toISOString().slice(0, 10);
+
+    const [result] = await connection.query(
+      "INSERT INTO todo (title, description, due_date, `order`) VALUES (?, ?, ?, ?)",
+      [title, description, due_date || now, order]
+    );
+
+    await connection.commit();
+
+    res.status(201).json({
+      message: `สร้าง ToDo '${title}' สำเร็จ`,
+      todo: {
+        id: result.insertId,
+        title,
+        description,
+        due_date: due_date
+          ? new Date(due_date).toISOString().slice(0, 10)
+          : now,
+        order,
+      },
+    });
+  } catch (error) {
+    if (connection) {
+      await connection.rollback();
+    }
+    next(error);
+  } finally {
+    connection.release();
+  }
+});
+
 app.delete("/todo/:id", async (req, res, next) => {
-  connection = await pool.getConnection();
+  let connection = await pool.getConnection();
   await connection.beginTransaction();
   try {
     const { id } = req.params;
+    const [[todo]] = await connection.query(
+      "SELECT title FROM todo WHERE id = ?",
+      [id]
+    );
     const [result] = await connection.query("DELETE FROM todo WHERE id = ?", [
       id,
     ]);
@@ -54,7 +84,7 @@ app.delete("/todo/:id", async (req, res, next) => {
       });
     }
     res.status(200).json({
-      message: "ลบ ToDo 'อ่านหนังสือสอบ Web Pro' สำเร็จ",
+      message: `ลบ ToDo '${todo.title}' สำเร็จ`,
     });
   } catch (error) {
     if (connection) {
@@ -62,47 +92,52 @@ app.delete("/todo/:id", async (req, res, next) => {
     }
     next(error);
   } finally {
-      connection.release();
+    connection.release();
   }
 });
 
 app.get("/todo", async (req, res, next) => {
-    connection = await pool.getConnection();
-    await connection.beginTransaction();
-    try {
-      const { start_date, end_date } = req.query;
-      let query = "SELECT * FROM todo";
-      let queryParams = [];
-        
-      if (start_date && end_date) {
-        query += " WHERE due_date BETWEEN ? AND ?";
-        queryParams = [start_date, end_date];
-      } else if (start_date) {
-        query += " WHERE due_date >= ?";
-        queryParams = [start_date];
-      } else if (end_date) {
-        query += " WHERE due_date <= ?";
-        queryParams = [end_date];
-      }
-  
-      query += " ORDER BY `order` ASC";
-  
-      const [result] = await connection.query(query, queryParams);
-      await connection.commit();
-      const formattedTodos = result.map((todo) => ({
-        ...todo,
-        due_date: new Date(todo.due_date).toISOString().slice(0, 10),
-      }));
-      res.status(200).json({ todos: formattedTodos });
-    } catch (error) {
-      if (connection) {
-        await connection.rollback();
-      }
-      next(error);
-    } finally {
-      connection.release();
+  let connection = await pool.getConnection();
+  await connection.beginTransaction();
+  try {
+    const { start_date, end_date } = req.query;
+    let query = "SELECT * FROM todo";
+    let queryParams = [];
+
+    if (start_date && end_date) {
+      query += " WHERE due_date BETWEEN ? AND ?";
+      queryParams = [start_date, end_date];
+    } else if (start_date) {
+      query += " WHERE due_date >= ?";
+      queryParams = [start_date];
+    } else if (end_date) {
+      query += " WHERE due_date <= ?";
+      queryParams = [end_date];
     }
-  });
-  
+
+    query += " ORDER BY `order` ASC";
+
+    const [result] = await connection.query(query, queryParams);
+    await connection.commit();
+
+    const formattedTodos = result.map((todo) => {
+      const dueDate = new Date(todo.due_date);
+      dueDate.setDate(dueDate.getDate() + 1); // add one day to due date
+      return {
+        ...todo,
+        due_date: dueDate.toISOString().slice(0, 10),
+      };
+    });
+
+    res.status(200).json(formattedTodos);
+  } catch (error) {
+    if (connection) {
+      await connection.rollback();
+    }
+    next(error);
+  } finally {
+    connection.release();
+  }
+});
 
 module.exports = app;
